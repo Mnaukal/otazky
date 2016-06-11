41cfcc31d5b2403363a78985bb00a18d132f6c77
Mnaukal <michal.topfer@gmail.com>
2016-06-11 20:19:50 +0200

    parsing questions - not complete

--- otazky.js
+++ otazky.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var questions = [];
+
 function readSingleFile(e) {
     var file = e.target.files[0];
     if (!file) {
@@ -28,4 +30,65 @@ function readFile(fileName)
 function displayContents(contents) {
     var element = document.getElementById('file-content');
     element.innerHTML = contents;
+    textToArray(contents)
+}
+
+function textToArray(text)
+{
+    var lines = text.split("\n");
+    var read = 0;
+    // 0 -> number
+    // 1 -> question
+    // 2 -> correct answer
+    // 3-6 -> Answers
+    
+    var question = {};
+        
+    for (var i = 0; i < lines.length; i++)
+    {
+        if(lines[i].length <= 2 || lines[i] == " " || lines[i] == "")
+        {
+            continue;
+        }
+        
+        if(read == 0)
+        {
+            question.number = parseInt(lines[i]);
+            read = 1;
+        }
+        else if(read == 1)
+        {
+            question.question = lines[i];
+            read = 2;
+        }
+        else if(read == 2)
+        {
+            question.correct = lines[i];
+            read = 3;
+        }
+        else if(read == 3)
+        {
+            question.A = lines[i];
+            read = 4;
+        }
+        else if(read == 4)
+        {
+            question.B = lines[i];
+            read = 5;
+        }
+        else if(read == 5)
+        {   
+            question.C = lines[i];
+            read = 6;
+        }
+        else if(read == 6)
+        {   
+            question.D = lines[i];
+            questions.push(question);
+            console.log(question);
+            read = 0;
+        }
+    }
+    
+    console.log(questions.length);
 }
